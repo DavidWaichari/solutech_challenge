e9d258bb19bdece8cfe8593c7801e692df847b58
dwaichari <dwaichari@gmail.com>
2023-04-18 12:29:41 +0300

    add field fields on models

--- app/Models/UserTask.php
+++ app/Models/UserTask.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class UserTask extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'user_id',
+        'task_id',
+        'due_date',
+        'start_time',
+        'end_time',
+        'remarks',
+        'status_id',
+        'deleted_at',
+    ];
 }
