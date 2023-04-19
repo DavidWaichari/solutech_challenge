9b18fbcc0bc67000a0f9cb8608edc613b0df4848
dwaichari <dwaichari@gmail.com>
2023-04-19 18:39:35 +0300

    update app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,30 +1,27 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Routing\UrlGenerator;
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        if (env('APP_ENV') !== 'local') {
-            $url->forceScheme('https');
-      }
-    }
+     /**
+      * Bootstrap any application services.
+      *
+      * @return void
+      */
+      public function boot(UrlGenerator $url)
+      {
+              if (env('APP_ENV') !== 'local') {
+                    $url->forceScheme('https');
+              }
+       }
+      /**
+       * Register any application services.
+       *
+       * @return void
+       */
+       public function register()
+       {
+              //
+       }
 }
