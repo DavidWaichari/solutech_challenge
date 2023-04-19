c59edebd0dc8a1f30e8f3f26ba55975b91ebe4ee
dwaichari <dwaichari@gmail.com>
2023-04-19 19:44:09 +0300

    config vite for production

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,38 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
-    assetsDir: 'admin',
-    plugins: [
-        vue(),
-        laravel([
-            'resources/sass/app.scss',
-            'resources/js/app.js',
-        ]),
-    ],
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            '@': '/resources/js',
-        }
+  plugins: [
+    vue(),
+    laravel([
+      'resources/sass/app.scss',
+      'resources/js/app.js',
+    ]),
+  ],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@': '/resources/js',
     },
-    build: {
-        rollupOptions: {
-            external: [
-                // externalize all image files and assets under the admin folder
-                /^\/admin\/(.+)\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/i,
-            ],
-        }
-    }
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        // externalize all image files and assets under the admin folder
+        /^\/admin\/(.+)\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/i,
+      ],
+    },
+    base: '/solutech-challenge.herokuapp.com/',
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://solutech-challenge.herokuapp.com',
+        changeOrigin: true,
+      },
+    },
+  },
 });
