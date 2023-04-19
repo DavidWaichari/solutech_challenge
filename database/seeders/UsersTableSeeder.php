724d05e3b120adbfa1fe2692b3ff0a7b147e47f2
dwaichari <dwaichari@gmail.com>
2023-04-19 18:07:33 +0300

    add seeder for one user

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //create an admin for solutech
+        //check if david exists
+        $user = User::where('email', 'dwaichari@gmail.com')->first();
+
+        if ($user == '') {
+            DB::table('users')->insert([
+                'name'=>'David Waichari',
+                'email'=>'dwaichari@gmail.com',
+                'password' => Hash::make('secret1234'),
+            ]);
+        }
+    }
+}
