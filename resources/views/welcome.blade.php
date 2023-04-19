eed808d245fb2cd1f7adbc3de11e76d1ef818048
dwaichari <dwaichari@gmail.com>
2023-04-19 20:14:55 +0300

    remove chart js imports

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,6 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
-     {{-- <!-- Custom styles for this page -->
-     <link href="admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"> --}}
 
     @vite(['resources/js/app.js'])
 </head>
@@ -63,20 +61,6 @@
 
     <!-- Custom scripts for all pages-->
     <script src="{{asset('admin/js/sb-admin-2.min.js')}}"></script>
-
-    <!-- Page level plugins -->
-    <script src="{{asset('admin/vendor/chart.js')}}/Chart.min.js')}}"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="{{asset('admin/js/demo/chart-area-demo.js')}}"></script>
-    <script src="{{asset('admin/js/demo/chart-pie-demo.js')}}"></script>
-
-     <!-- Page level plugins -->
-     <script src="{{asset('admin/vendor/datatables/jquery.dataTables.min.js')}}"></script>
-     <script src="{{asset('admin/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
- 
-     <!-- Page level custom scripts -->
-     <script src="{{asset('admin/js/demo/datatables-demo.js')}}"></script>
 
 </body>
 
