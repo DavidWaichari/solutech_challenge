db68577e49753278d5948cff00e01fed6a28bd5a
dwaichari <dwaichari@gmail.com>
2023-04-20 20:52:15 +0300

    install and configure apex chart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import './bootstrap';
 import '../sass/app.scss'
 import Router from '@/router'
+import VueApexCharts from 'vue3-apexcharts';
 
 import { createApp } from 'vue/dist/vue.esm-bundler';
 
 const app = createApp({})
+app.component('apexchart', VueApexCharts);
+
 app.use(Router)
 app.mount('#app')
---
+++ resources/js/components/ApexChart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <apexchart type="line" :options="chartOptions" :series="chartSeries" />
+    </div>
+</template>
+  
+  <script>
+  export default {
+    name: 'LineChart',
+    data() {
+      return {
+        chartOptions: {
+          chart: {
+            id: 'line-chart',
+          },
+          xaxis: {
+            categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          },
+        },
+        chartSeries: [
+          {
+            name: 'My Series',
+            data: [30, 40, 25, 50, 49, 21, 70],
+          },
+        ],
+      };
+    },
+  };
+  </script>
+  
